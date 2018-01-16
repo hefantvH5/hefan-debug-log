49bed120c1461c624e32c08476f3788d9a560ec2
guozongwei <guozongwei@hefantv.com>
2018-01-16 18:34:54 +0800

    改版，加入配置

--- index-es6.js
+++ index-es6.js
@@ -1,156 +1,134 @@
 /**
  * Created by Cray on 2016/4/29.
  */
-let projectName = typeof _PROJECTNAME !== 'undefined' && _PROJECTNAME ? _PROJECTNAME : '项目名称未配置';
-let _LOGENV = _LOGENV || 'null'
-let env = __DEV ? '测试环境（process.env==' + _LOGENV + '）' : '预上线/正式环境（process.env==' + _LOGENV + '）';
-const LEVEL_CONFIG = { 'debug': true, 'log': true, 'info': true, 'warn': true, 'error': true };
+let created = false;
+let instance = null;
+class Log {
+    constructor() {
+        this.envArray = ['development', 'testing', 'preproduction', 'production'];
 
-const Log = {
-    startup(value = 'debug') {
-        let array = ['debug', 'log', 'info', 'warn', 'error'];
-        // let enable = false;
-        // array.forEach((type) => {
-        //     type == value ? (enable = true) : (LEVEL_CONFIG[type] = false);
+        if (!created) {
+            created = true;
+            this._jsErrorHandler()
+            instance = new Log();
+        }
+        this.config()
 
-        //     if (enable) {
-        //         LEVEL_CONFIG[type] = true;
-        //     }
-        // });
+        return instance
+    }
 
-    },
+    config(pN, env = process.env.NODE_ENV, value = 'debug') {
+        this.projectName = pN || typeof _PROJECTNAME !== 'undefined' && _PROJECTNAME ? _PROJECTNAME : '项目名称未配置';
+        this.enable = true;
+        this.typeArray = ['debug', 'log', 'info', 'warn', 'error'];
+        let envIndex = this.envArray.indexOf(env);
+        if (envIndex > -1) {
+            this.env = env
+        } else {
+            this.enable = false;
+        }
+        let typeIndex = this.typeArray.indexOf(value);
+        if (typeIndex > -1) {
+            this.typeArray = this.typeArray.splice(typeIndex)
+        }
 
+    }
     debug(pageName, ...msg) {
-        _consolePrint('debug', pageName, msg);
-    },
+        this._consolePrint('debug', pageName, msg);
+    }
 
     log(pageName, ...msg) {
-        _consolePrint('log', pageName, msg);
-    },
+        this._consolePrint('log', pageName, msg);
+    }
 
     info(pageName, ...msg) {
-        _consolePrint('info', pageName, msg);
-    },
+        this._consolePrint('info', pageName, msg);
+    }
 
     warn(pageName, ...msg) {
-        _consolePrint('warn', pageName, msg);
-    },
+        this._consolePrint('warn', pageName, msg);
+    }
 
     error(pageName, ...msg) {
-        _consolePrint('error', pageName, msg);
+        this._consolePrint('error', pageName, msg);
     }
-
-}
-
-if (__DEV) {
-    Log.startup('debug')
-} else {
-    Log.startup('log')
-}
-
-function _ajax(url, data) {
-    // 创建ajax对象
-    var xhr = null;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        xhr = new ActiveXObject('Microsoft.XMLHTTP')
-    }
-    // 用于清除缓存
-    var random = Math.random();
-
-    if (typeof data == 'object') {
-        var str = '';
-        for (var key in data) {
-            str += key + '=' + data[key] + '&';
+    _ajax(url, data) {
+        // 创建ajax对象
+        var xhr = null;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            xhr = new ActiveXObject('Microsoft.XMLHTTP')
         }
-        data = str.replace(/&$/, '');
-    }
-
-
-    xhr.open('POST', url, true);
-    // 如果需要像 html 表单那样 POST 数据，请使用 setRequestHeader() 来添加 http 头。
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(data);
-
-
-    // 处理返回数据
-    // xhr.onreadystatechange = function() {
-    //     if (xhr.readyState == 4) {
-    //         if (xhr.status == 200) {
-    //             success(xhr.responseText);
-    //         } else {
-    //             if (failed) {
-    //                 failed(xhr.status);
-    //             }
-    //         }
-    //     }
-    // }
-}
-
-
-function _consolePrint(type, pageName, msg) {
-    if (LEVEL_CONFIG[type]) {
-        const fn = window.console[type];
-        if (fn) {
-            fn.apply(window.console, _formatMsg(type, msg));
-            _debugImg(type, pageName, msg)
+        // 用于清除缓存
+        var random = Math.random();
+        if (typeof data == 'object') {
+            var str = '';
+            for (var key in data) {
+                str += key + '=' + data[key] + '&';
+            }
+            data = str.replace(/&$/, '');
         }
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(data);
     }
-}
-
-function _debugImg(type, pageName, data) {
-    let imgData = _paramFormat({ "projectName": projectName, "type": type, env: env, "action": "4001", "pageName": pageName, "logData": data });
-    // let img = new Image();
-    // img.src = 'http://debug.hefantv.com/debug.gif?data=' + encodeURIComponent(imgData.data);
-
-    // 测试调用
-
-    _ajax('http://debug.hefantv.com/api/postDebug', imgData);
-}
-
-function _getTime() {
-    let d = new Date();
-    return String(d.getHours()) + ":" + String(d.getMinutes()) + ":" + String(d.getSeconds());
-}
-
-function _paramFormat(data) {
-    let result = {};
-    result.data = JSON.stringify(data);
-    return result;
-}
-if (typeof window !== 'undefined') {
-    window.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
-        let errorInfo = {
-            errorMessage: {
-                meaning: '错误信息：',
-                msg: errorMessage
-            },
-            scriptURI: {
-                meaning: '出错文件：',
-                msg: scriptURI
-            },
-            lineNumber: {
-                meaning: '出错行号：',
-                msg: lineNumber
-            },
-            columnNumber: {
-                meaning: '出错列号：',
-                msg: columnNumber
-            },
-            errorObj: {
-                meaning: '错误详情：',
-                msg: errorObj
+    _consolePrint(type, pageName, msg) {
+        if (this.enable && this.typeArray.indexOf(type) > -1) {
+            const fn = window.console[type];
+            if (fn) {
+                fn.apply(window.console, this._formatMsg(type, msg));
+                this._debugHandler(type, pageName, msg)
             }
         }
-        _debugImg('error', scriptURI, errorInfo)
+    }
+    _debugHandler(type, pageName, data) {
+        let imgData = this._paramFormat({ "projectName": this.projectName, "type": type, env: this.env, "action": "4001", "pageName": pageName, "logData": data });
+        this._ajax('http://debug.hefantv.com/api/postDebug', imgData);
+    }
+    _getTime() {
+        let d = new Date();
+        return String(d.getHours()) + ":" + String(d.getMinutes()) + ":" + String(d.getSeconds());
+    }
+    _paramFormat(data) {
+        let result = {};
+        result.data = JSON.stringify(data);
+        return result;
+    }
+    _formatMsg(type, msg) {
+        msg.unshift(this._getTime() + ' [' + type + '] > ');
+        return msg;
+    }
+    _jsErrorHandler() {
+        let _this = this;
+        if (typeof window !== 'undefined' && !window.onerror) {
+            window.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
+                let errorInfo = {
+                    errorMessage: {
+                        meaning: '错误信息：',
+                        msg: errorMessage
+                    },
+                    scriptURI: {
+                        meaning: '出错文件：',
+                        msg: scriptURI
+                    },
+                    lineNumber: {
+                        meaning: '出错行号：',
+                        msg: lineNumber
+                    },
+                    columnNumber: {
+                        meaning: '出错列号：',
+                        msg: columnNumber
+                    },
+                    errorObj: {
+                        meaning: '错误详情：',
+                        msg: errorObj
+                    }
+                }
+                _this._debugHandler('error', scriptURI, errorInfo)
+            }
+        }
     }
 }
 
-
-function _formatMsg(type, msg) {
-    msg.unshift(_getTime() + ' [' + type + '] > ');
-    return msg;
-}
-
-module.exports = Log;
+module.exports = new Log();
--- index.js
+++ index.js
@@ -2,170 +2,191 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /**
  * Created by Cray on 2016/4/29.
  */
-var projectName = typeof _PROJECTNAME !== 'undefined' && _PROJECTNAME ? _PROJECTNAME : '项目名称未配置';
-var _LOGENV = _LOGENV || 'null';
-var env = __DEV ? '测试环境（process.env==' + _LOGENV + '）' : '预上线/正式环境（process.env==' + _LOGENV + '）';
-var LEVEL_CONFIG = { 'debug': true, 'log': true, 'info': true, 'warn': true, 'error': true };
+var created = false;
+var instance = null;
 
-var Log = {
-    startup: function startup() {
-        var value = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'debug';
+var Log = function () {
+    function Log() {
+        _classCallCheck(this, Log);
 
-        var array = ['debug', 'log', 'info', 'warn', 'error'];
-        // let enable = false;
-        // array.forEach((type) => {
-        //     type == value ? (enable = true) : (LEVEL_CONFIG[type] = false);
+        this.envArray = ['development', 'testing', 'preproduction', 'production'];
 
-        //     if (enable) {
-        //         LEVEL_CONFIG[type] = true;
-        //     }
-        // });
-    },
-    debug: function debug(pageName) {
-        for (var _len = arguments.length, msg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-            msg[_key - 1] = arguments[_key];
+        if (!created) {
+            created = true;
+            this._jsErrorHandler();
+            instance = new Log();
         }
+        this.config();
 
-        _consolePrint('debug', pageName, msg);
-    },
-    log: function log(pageName) {
-        for (var _len2 = arguments.length, msg = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-            msg[_key2 - 1] = arguments[_key2];
-        }
-
-        _consolePrint('log', pageName, msg);
-    },
-    info: function info(pageName) {
-        for (var _len3 = arguments.length, msg = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
-            msg[_key3 - 1] = arguments[_key3];
-        }
-
-        _consolePrint('info', pageName, msg);
-    },
-    warn: function warn(pageName) {
-        for (var _len4 = arguments.length, msg = Array(_len4 > 1 ? _len4 - 1 : 0), _key4 = 1; _key4 < _len4; _key4++) {
-            msg[_key4 - 1] = arguments[_key4];
-        }
-
-        _consolePrint('warn', pageName, msg);
-    },
-    error: function error(pageName) {
-        for (var _len5 = arguments.length, msg = Array(_len5 > 1 ? _len5 - 1 : 0), _key5 = 1; _key5 < _len5; _key5++) {
-            msg[_key5 - 1] = arguments[_key5];
-        }
-
-        _consolePrint('error', pageName, msg);
-    }
-};
-
-if (__DEV) {
-    Log.startup('debug');
-} else {
-    Log.startup('log');
-}
-
-function _ajax(url, data) {
-    // 创建ajax对象
-    var xhr = null;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        xhr = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    // 用于清除缓存
-    var random = Math.random();
-
-    if ((typeof data === 'undefined' ? 'undefined' : _typeof(data)) == 'object') {
-        var str = '';
-        for (var key in data) {
-            str += key + '=' + data[key] + '&';
-        }
-        data = str.replace(/&$/, '');
+        return instance;
     }
 
-    xhr.open('POST', url, true);
-    // 如果需要像 html 表单那样 POST 数据，请使用 setRequestHeader() 来添加 http 头。
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(data);
+    _createClass(Log, [{
+        key: 'config',
+        value: function config(pN) {
+            var env = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : process.env.NODE_ENV;
+            var value = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'debug';
 
-    // 处理返回数据
-    // xhr.onreadystatechange = function() {
-    //     if (xhr.readyState == 4) {
-    //         if (xhr.status == 200) {
-    //             success(xhr.responseText);
-    //         } else {
-    //             if (failed) {
-    //                 failed(xhr.status);
-    //             }
-    //         }
-    //     }
-    // }
-}
-
-function _consolePrint(type, pageName, msg) {
-    if (LEVEL_CONFIG[type]) {
-        var fn = window.console[type];
-        if (fn) {
-            fn.apply(window.console, _formatMsg(type, msg));
-            _debugImg(type, pageName, msg);
-        }
-    }
-}
-
-function _debugImg(type, pageName, data) {
-    var imgData = _paramFormat({ "projectName": projectName, "type": type, env: env, "action": "4001", "pageName": pageName, "logData": data });
-    // let img = new Image();
-    // img.src = 'http://debug.hefantv.com/debug.gif?data=' + encodeURIComponent(imgData.data);
-
-    // 测试调用
-
-    _ajax('http://debug.hefantv.com/api/postDebug', imgData);
-}
-
-function _getTime() {
-    var d = new Date();
-    return String(d.getHours()) + ":" + String(d.getMinutes()) + ":" + String(d.getSeconds());
-}
-
-function _paramFormat(data) {
-    var result = {};
-    result.data = JSON.stringify(data);
-    return result;
-}
-if (typeof window !== 'undefined') {
-    window.onerror = function (errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
-        var errorInfo = {
-            errorMessage: {
-                meaning: '错误信息：',
-                msg: errorMessage
-            },
-            scriptURI: {
-                meaning: '出错文件：',
-                msg: scriptURI
-            },
-            lineNumber: {
-                meaning: '出错行号：',
-                msg: lineNumber
-            },
-            columnNumber: {
-                meaning: '出错列号：',
-                msg: columnNumber
-            },
-            errorObj: {
-                meaning: '错误详情：',
-                msg: errorObj
+            this.projectName = pN || typeof _PROJECTNAME !== 'undefined' && _PROJECTNAME ? _PROJECTNAME : '项目名称未配置';
+            this.enable = true;
+            this.typeArray = ['debug', 'log', 'info', 'warn', 'error'];
+            var envIndex = this.envArray.indexOf(env);
+            if (envIndex > -1) {
+                this.env = env;
+            } else {
+                this.enable = false;
             }
-        };
-        _debugImg('error', scriptURI, errorInfo);
-    };
-}
+            var typeIndex = this.typeArray.indexOf(value);
+            if (typeIndex > -1) {
+                this.typeArray = this.typeArray.splice(typeIndex);
+            }
+        }
+    }, {
+        key: 'debug',
+        value: function debug(pageName) {
+            for (var _len = arguments.length, msg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+                msg[_key - 1] = arguments[_key];
+            }
 
-function _formatMsg(type, msg) {
-    msg.unshift(_getTime() + ' [' + type + '] > ');
-    return msg;
-}
+            this._consolePrint('debug', pageName, msg);
+        }
+    }, {
+        key: 'log',
+        value: function log(pageName) {
+            for (var _len2 = arguments.length, msg = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+                msg[_key2 - 1] = arguments[_key2];
+            }
 
-module.exports = Log;
+            this._consolePrint('log', pageName, msg);
+        }
+    }, {
+        key: 'info',
+        value: function info(pageName) {
+            for (var _len3 = arguments.length, msg = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
+                msg[_key3 - 1] = arguments[_key3];
+            }
+
+            this._consolePrint('info', pageName, msg);
+        }
+    }, {
+        key: 'warn',
+        value: function warn(pageName) {
+            for (var _len4 = arguments.length, msg = Array(_len4 > 1 ? _len4 - 1 : 0), _key4 = 1; _key4 < _len4; _key4++) {
+                msg[_key4 - 1] = arguments[_key4];
+            }
+
+            this._consolePrint('warn', pageName, msg);
+        }
+    }, {
+        key: 'error',
+        value: function error(pageName) {
+            for (var _len5 = arguments.length, msg = Array(_len5 > 1 ? _len5 - 1 : 0), _key5 = 1; _key5 < _len5; _key5++) {
+                msg[_key5 - 1] = arguments[_key5];
+            }
+
+            this._consolePrint('error', pageName, msg);
+        }
+    }, {
+        key: '_ajax',
+        value: function _ajax(url, data) {
+            // 创建ajax对象
+            var xhr = null;
+            if (window.XMLHttpRequest) {
+                xhr = new XMLHttpRequest();
+            } else {
+                xhr = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            // 用于清除缓存
+            var random = Math.random();
+            if ((typeof data === 'undefined' ? 'undefined' : _typeof(data)) == 'object') {
+                var str = '';
+                for (var key in data) {
+                    str += key + '=' + data[key] + '&';
+                }
+                data = str.replace(/&$/, '');
+            }
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.send(data);
+        }
+    }, {
+        key: '_consolePrint',
+        value: function _consolePrint(type, pageName, msg) {
+            if (this.enable && this.typeArray.indexOf(type) > -1) {
+                var fn = window.console[type];
+                if (fn) {
+                    fn.apply(window.console, this._formatMsg(type, msg));
+                    this._debugHandler(type, pageName, msg);
+                }
+            }
+        }
+    }, {
+        key: '_debugHandler',
+        value: function _debugHandler(type, pageName, data) {
+            var imgData = this._paramFormat({ "projectName": this.projectName, "type": type, env: this.env, "action": "4001", "pageName": pageName, "logData": data });
+            this._ajax('http://debug.hefantv.com/api/postDebug', imgData);
+        }
+    }, {
+        key: '_getTime',
+        value: function _getTime() {
+            var d = new Date();
+            return String(d.getHours()) + ":" + String(d.getMinutes()) + ":" + String(d.getSeconds());
+        }
+    }, {
+        key: '_paramFormat',
+        value: function _paramFormat(data) {
+            var result = {};
+            result.data = JSON.stringify(data);
+            return result;
+        }
+    }, {
+        key: '_formatMsg',
+        value: function _formatMsg(type, msg) {
+            msg.unshift(this._getTime() + ' [' + type + '] > ');
+            return msg;
+        }
+    }, {
+        key: '_jsErrorHandler',
+        value: function _jsErrorHandler() {
+            var _this = this;
+            if (typeof window !== 'undefined' && !window.onerror) {
+                window.onerror = function (errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
+                    var errorInfo = {
+                        errorMessage: {
+                            meaning: '错误信息：',
+                            msg: errorMessage
+                        },
+                        scriptURI: {
+                            meaning: '出错文件：',
+                            msg: scriptURI
+                        },
+                        lineNumber: {
+                            meaning: '出错行号：',
+                            msg: lineNumber
+                        },
+                        columnNumber: {
+                            meaning: '出错列号：',
+                            msg: columnNumber
+                        },
+                        errorObj: {
+                            meaning: '错误详情：',
+                            msg: errorObj
+                        }
+                    };
+                    _this._debugHandler('error', scriptURI, errorInfo);
+                };
+            }
+        }
+    }]);
+
+    return Log;
+}();
+
+module.exports = new Log();
