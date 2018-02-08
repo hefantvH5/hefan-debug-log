ae6fc8f99d183c9337b7486da07eca6dac6f2f6e
李春伟 <lichunwei@hefantv.com>
2018-02-08 18:04:46 +0800

    for ip

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ var Log = function () {
 
         if (!created) {
             created = true;
-            this._jsErrorHandler();
             this._jsGetClientIP();
+            this._jsErrorHandler();
             instance = new Log();
         }
         this.config();
@@ -122,10 +122,24 @@ var Log = function () {
     }, {
         key: '_debugHandler',
         value: function _debugHandler(type, msg) {
-            var imgData = this._paramFormat({ "pjKey": this.pjKey, "type": type, server: "",
+            var _this = this;
+            var sourceData = { "pjKey": this.pjKey, "type": type, server: "",
                 env: this.env, "action": "4001", "url": window.location.href, "logData": msg,
-                ipInfo: window.returnCitySN || {} });
-            this._ajax('http://debug.hefantv.com/api/postDebug', imgData);
+                ipInfo: window.returnCitySN };
+            var imgData = this._paramFormat(sourceData);
+            if (window.returnCitySN) {
+                this._ajax('http://debug.hefantv.com/api/postDebug', imgData, 'POST');
+            } else {
+                this._ajax('http://h5api.hefantv.com/api/ipaddress').then(function (data) {
+                    try {
+                        sourceData.ipInfo = JSON.parse(data);
+                    } catch (e) {
+                        sourceData.ipInfo = {};
+                    }
+                    imgData = _this._paramFormat(sourceData);
+                    _this._ajax('http://debug.hefantv.com/api/postDebug', imgData, 'POST');
+                });
+            }
         }
     }, {
         key: '_getTime',
@@ -245,6 +259,8 @@ var Log = function () {
     }, {
         key: '_ajax',
         value: function _ajax(url, data) {
+            var method = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'GET';
+
             // 创建ajax对象
             var xhr = null;
             if (window.XMLHttpRequest) {
@@ -261,9 +277,22 @@ var Log = function () {
                 }
                 data = str.replace(/&$/, '');
             }
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.send(data);
+            return new Promise(function (resolve, reject) {
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        resolve(xhr.responseText);
+                    }
+                };
+
+                if (method == 'POST') {
+                    xhr.open('POST', url, true);
+                    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    xhr.send(data);
+                } else {
+                    xhr.open('GET', url, true);
+                    xhr.send();
+                }
+            });
         }
     }]);
 
